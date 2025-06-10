df0f285a93aa19cf5803bc283c7f4ff9903db825
tirthapriya12 <tirthapriya.banerjee@gmail.com>
2025-06-11 00:44:08 +0530

    mockdata test update

--- src/__mocks__/apiData.ts
+++ src/__mocks__/apiData.ts
@@ -257,7 +257,7 @@ export const mockProducts: Product[] = [
   {
     id: 20,
     title: "DANVOUY Womens T Shirt Casual Cotton Short",
-    price: 12.99,
+    price: 13.99,
     description:
       "95%Cotton,5%Spandex, Features: Casual, Short Sleeve, Letter Print,V-Neck,Fashion Tees, The fabric is soft and has some stretch., Occasion: Casual/Office/Beach/School/Home/Street. Season: Spring,Summer,Autumn,Winter.",
     category: "women's clothing",
